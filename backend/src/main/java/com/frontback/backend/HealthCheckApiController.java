8fcb10a745474b010a795f0d42485d245f308560
zhtcks <zhtcks@gmail.com>
2022-05-06 19:00:55 +0900

    add: CustomValue property 검증 코드 추가

--- backend/src/main/java/com/frontback/backend/HealthCheckApiController.java
+++ backend/src/main/java/com/frontback/backend/HealthCheckApiController.java
@@ -1,5 +1,6 @@
 package com.frontback.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,16 @@ public class HealthCheckApiController {
     @Value("${current.profile}")
     private String profile;
 
+    @Autowired
+    private CustomConfig customConfig;
+
     @GetMapping
     public String getSystemTimeMilli() {
         return String.format("active profile: %s, %d", profile, System.currentTimeMillis());
+    }
+
+    @GetMapping("/v2")
+    public String getSystemTimeMilli_v2() {
+        return String.format("my-custom-value: %s, %d", customConfig.toString(), System.currentTimeMillis());
     }
 }
