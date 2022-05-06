f677a3eec48e21bc1ec7d9cc292f3524bdfc6ddb
zhtcks <zhtcks@gmail.com>
2022-05-06 20:11:35 +0900

    update: docker-compose down 옵션 추가

--- scripts/deploy-sandbox.sh
+++ scripts/deploy-sandbox.sh
@@ -23,5 +23,8 @@ then
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
+echo "start docker-compose down: ubuntu"
+sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose-sandbox.yml down
+
 echo "start docker-compose up: ubuntu"
 sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose-sandbox.yml up --build -d
